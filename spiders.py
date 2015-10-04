890af8f9891e008add9fbacb29379df4296ebcff
c475 <danjamesbond@gmail.com>
2015-10-03 22:44:24 -0700

    .

--- spiders.py
+++ spiders.py
@@ -4,14 +4,22 @@ from random import choice, randint
 
 from flask import Flask, request, g, render_template
 
+# config
+DEBUG = False
+
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 @app.before_request
 def before_request():
     words = []
     for i in xrange(0, randint(33, 133)):
+        word = []
+        for j in xrange(0, 4):
+            word.append(choice(coolwords))
+
         words.append({
-            'word': [choice(coolwords), choice(coolwords)]
+            'word': ['/'.join(word), choice(coolwords)]
         })
 
     g.words = words
@@ -19,4 +27,14 @@ def before_request():
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
+    if path == 'robots.txt':
+        f = open('robots.txt')
+        c = f.read()
+        f.close()
+        return c
+
     return render_template('index.html', words=g.words)
+
+
+if __name__ == '__main__':
+    app.run()
